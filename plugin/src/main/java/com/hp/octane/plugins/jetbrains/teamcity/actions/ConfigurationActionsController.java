2dd9bc61b8c422bac831f63e000c4bdf73357ea9
lokshin <evgene.lokshin@microfocus.com>
2019-01-21 09:48:18 +0200

    1. Defect:  #781082 Team City configuration section has redundant field "Shared Space" 2. Defect: #781086 Team City plugin configuration the URL should be in specific format and include the shared space query param 3. Fix delete configuration scenario

--- plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/actions/ConfigurationActionsController.java
+++ plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/actions/ConfigurationActionsController.java
@@ -23,13 +23,13 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.hp.octane.integrations.OctaneConfiguration;
 import com.hp.octane.integrations.OctaneSDK;
 import com.hp.octane.plugins.jetbrains.teamcity.TeamCityPluginServicesImpl;
-import com.hp.octane.plugins.jetbrains.teamcity.configuration.OctaneConfigMultiSharedSpaceStructure;
-import com.hp.octane.plugins.jetbrains.teamcity.configuration.OctaneConfigStructure;
-import com.hp.octane.plugins.jetbrains.teamcity.configuration.TCConfigurationHolder;
-import com.hp.octane.plugins.jetbrains.teamcity.configuration.TCConfigurationService;
+import com.hp.octane.plugins.jetbrains.teamcity.configuration.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -37,6 +37,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.*;
 
 import static com.hp.octane.plugins.jetbrains.teamcity.utils.Utils.buildResponseStringEmptyConfigs;
@@ -46,180 +49,216 @@ import static com.hp.octane.plugins.jetbrains.teamcity.utils.Utils.buildResponse
  */
 
 public class ConfigurationActionsController implements Controller {
-    private static final Logger logger = LogManager.getLogger(ConfigurationActionsController.class);
+	private static final Logger logger = LogManager.getLogger(ConfigurationActionsController.class);
+	private static final String PARAM_SHARED_SPACE = "p"; // NON-NLS
+	@Autowired
+	private TCConfigurationService configurationService;
+	@Autowired
+	private TCConfigurationHolder holder;
 
-    @Autowired
-    private TCConfigurationService configurationService;
-    @Autowired
-    private TCConfigurationHolder holder;
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+		String returnStr = "";
+		String action = httpServletRequest.getParameter("action");
 
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        String returnStr = "";
-        String action = httpServletRequest.getParameter("action");
+		if (!"post".equals(httpServletRequest.getMethod().toLowerCase()) && (action == null || action.isEmpty())) {
+			returnStr = reloadConfiguration();
+		} else {
+			try {
+				if ("test".equalsIgnoreCase(action)) {
+					String server = httpServletRequest.getParameter("server");
+					MqmProject project = parseUiLocation(server);
+					if (project.hasError()) {
+						returnStr = buildResponseStringEmptyConfigs(project.getErrorMsg());
+					} else {
+						String apiKey = httpServletRequest.getParameter("username");
+						String secret = httpServletRequest.getParameter("password");
 
-        if (!"post".equals(httpServletRequest.getMethod().toLowerCase()) && (action == null || action.isEmpty())) {
-            returnStr = reloadConfiguration();
-        } else {
-            try {
-                if ("test".equalsIgnoreCase(action)) {
-                    String server = httpServletRequest.getParameter("server");
-                    String url = parseUiLocation(server);
-                    String apiKey = httpServletRequest.getParameter("username");
-                    String secret = httpServletRequest.getParameter("password");
-                    String sharedSpace = httpServletRequest.getParameter("sharedSpace");
-                    OctaneConfiguration testedOctaneConfiguration = new OctaneConfiguration(UUID.randomUUID().toString(), url, sharedSpace);
-                    testedOctaneConfiguration.setClient(apiKey);
-                    testedOctaneConfiguration.setSecret(secret);
-                    returnStr = configurationService.checkConfiguration(testedOctaneConfiguration);
-                } else {
-                    //save configuration
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    TypeFactory typeFactory = objectMapper.getTypeFactory();
-                    CollectionType collectionType = typeFactory.constructCollectionType(
-                            List.class, OctaneConfigStructure.class);
-                    List<OctaneConfigStructure> configs = objectMapper.readValue(httpServletRequest.getInputStream(), collectionType);
-                    handleDeletedConfigurations(configs);
-                    returnStr = updateConfiguration(configs);
-                }
-            } catch (Exception e) {
-                logger.error("failed to process configuration request (" + (action == null ? "save" : action) + ")", e);
-                returnStr = e.getMessage() + ". Failed to process configuration request (" + (action == null ? "save" : action) + ")";
-                returnStr = buildResponseStringEmptyConfigs(returnStr);
-            }
-        }
+						OctaneConfiguration testedOctaneConfiguration = new OctaneConfiguration(UUID.randomUUID().toString(),
+								project.getLocation(),
+								project.getSharedSpace());
+						testedOctaneConfiguration.setClient(apiKey);
+						testedOctaneConfiguration.setSecret(secret);
+						returnStr = configurationService.checkConfiguration(testedOctaneConfiguration);
+					}
+				} else {
+					//save configuration
+					ObjectMapper objectMapper = new ObjectMapper();
+					TypeFactory typeFactory = objectMapper.getTypeFactory();
+					CollectionType collectionType = typeFactory.constructCollectionType(
+							List.class, OctaneConfigStructure.class);
+					List<OctaneConfigStructure> configs = objectMapper.readValue(httpServletRequest.getInputStream(), collectionType);
+					handleDeletedConfigurations(configs);
+					returnStr = updateConfiguration(configs);
+				}
+			} catch (Exception e) {
+				logger.error("failed to process configuration request (" + (action == null ? "save" : action) + ")", e);
+				returnStr = e.getMessage() + ". Failed to process configuration request (" + (action == null ? "save" : action) + ")";
+				returnStr = buildResponseStringEmptyConfigs(returnStr);
+			}
+		}
 
-        PrintWriter writer;
-        try {
-            writer = httpServletResponse.getWriter();
-            writer.write(returnStr);
-        } catch (IOException ioe) {
-            logger.error("failed to write response", ioe);
-        }
-        return null;
-    }
+		PrintWriter writer;
+		try {
+			writer = httpServletResponse.getWriter();
+			writer.write(returnStr);
+		} catch (IOException ioe) {
+			logger.error("failed to write response", ioe);
+		}
+		return null;
+	}
 
-    private void handleDeletedConfigurations(List<OctaneConfigStructure> newConfigs) {
-        Map<String, OctaneConfiguration> origConfigs = holder.getOctaneConfigurations();
-        Set<String> configToRemove = new HashSet<>();
-        origConfigs.entrySet().forEach(entry -> {
-            boolean found = false;
-            for (OctaneConfigStructure newConfig : newConfigs) {
-                String identity = newConfig.getIdentity();
-                //new configuration
-                if (identity == null || identity.isEmpty() || "undefined".equalsIgnoreCase(identity)) {
-                    found = true;
-                    break;
-                }
+	private void handleDeletedConfigurations(List<OctaneConfigStructure> newConfigs) {
+		Map<String, OctaneConfiguration> origConfigs = holder.getOctaneConfigurations();
+		Set<String> newConfigIdentities = new HashSet<>();
+		for (OctaneConfigStructure conf : newConfigs) {
+			String identity = conf.getIdentity() == null || conf.getIdentity().isEmpty() ? "empty" : conf.getIdentity();
+			newConfigIdentities.add(identity);
+		}
+		Set<String> configToRemove = new HashSet<>();
+		for (String identity : origConfigs.keySet()) {
+			if (!newConfigIdentities.contains(identity)){
+				logger.info("Removing client with instance Id: " + identity);
+				OctaneSDK.removeClient(OctaneSDK.getClientByInstanceId(identity));
+				configToRemove.add(identity);
+			}
+		}
 
-                if (entry.getKey().equals(newConfig.getIdentity())) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                OctaneConfiguration octaneConfiguration = entry.getValue();
-                if (octaneConfiguration != null) {
-                    logger.info("Removing client with instance Id: " + entry.getKey());
-                    OctaneSDK.removeClient(OctaneSDK.getClientByInstanceId(entry.getKey()));
-                    configToRemove.add(entry.getKey());
-                }
-            }
-        });
-        if (!configToRemove.isEmpty()) {
-            configToRemove.forEach(instanceId -> origConfigs.remove(instanceId));
-            //remove config before save
-            for (OctaneConfigStructure octaneConfigStructure : new ArrayList<>(holder.getConfigs())) {
-                if (configToRemove.contains(octaneConfigStructure.getIdentity())) {
-                    holder.getConfigs().remove(octaneConfigStructure);
-                }
-            }
-            save();
-        }
-    }
+		if (!configToRemove.isEmpty()) {
+			configToRemove.forEach(instanceId -> origConfigs.remove(instanceId));
+			//remove config before save
+			for (OctaneConfigStructure octaneConfigStructure : new ArrayList<>(holder.getConfigs())) {
+				if (configToRemove.contains(octaneConfigStructure.getIdentity())) {
+					holder.getConfigs().remove(octaneConfigStructure);
+				}
+			}
+			save();
+		}
+	}
 
-    private String save() {
-        logger.info("Saving ALM Octane configurations...");
-        OctaneConfigMultiSharedSpaceStructure confs = new OctaneConfigMultiSharedSpaceStructure();
-        confs.setMultiConfigStructure(holder.getConfigs());
-        return configurationService.saveConfig(confs);
-    }
+	private String save() {
+		logger.info("Saving ALM Octane configurations...");
+		OctaneConfigMultiSharedSpaceStructure confs = new OctaneConfigMultiSharedSpaceStructure();
+		for (OctaneConfigStructure conf : holder.getConfigs()) {
+			conf.setSecretPassword(conf.getSecretPassword());
+		}
+		confs.setMultiConfigStructure(holder.getConfigs());
+		return configurationService.saveConfig(confs);
+	}
 
-    public String updateConfiguration(List<OctaneConfigStructure> newConfigs) {
-        List<OctaneConfigStructure> originalConfigs = holder.getConfigs();
-        for (OctaneConfigStructure newConf : newConfigs) {
-            OctaneConfigStructure result = originalConfigs.stream()
-                    .filter(or_conf -> or_conf.getIdentity().equals(newConf.getIdentity()))
-                    .findAny()
-                    .orElse(null);
-            if (result == null) {
-                checkAndUpdateIdentityAndLocationIfNotTheSame(newConf);
-                OctaneConfiguration octaneConfiguration = new OctaneConfiguration(newConf.getIdentity(), newConf.getLocation(),
-                        newConf.getSharedSpace());
-                octaneConfiguration.setClient(newConf.getUsername());
-                octaneConfiguration.setSecret(newConf.getSecretPassword());
-                try {
-                    OctaneSDK.addClient(octaneConfiguration, TeamCityPluginServicesImpl.class);
-                }catch (Exception e){
-                   return  buildResponseStringEmptyConfigs(e.getMessage());
-                }
-                holder.getOctaneConfigurations().put(newConf.getIdentity(), octaneConfiguration);
-                holder.getConfigs().add(newConf);
-            } else {
-                //update existing configuration
-                OctaneConfiguration octaneConfiguration = holder.getOctaneConfigurations().get(result.getIdentity());
-                octaneConfiguration.setUrl(newConf.getUiLocation());
-                result.setUiLocation(newConf.getUiLocation());
-                octaneConfiguration.setClient(newConf.getUsername());
-                result.setUsername(newConf.getUsername());
-                octaneConfiguration.setSecret(newConf.getSecretPassword());
-                result.setSecretPassword(newConf.getSecretPassword());
-                octaneConfiguration.setSharedSpace(newConf.getSharedSpace());
-                result.setSharedSpace(newConf.getSharedSpace());
-                result.setLocation(parseUiLocation(newConf.getUiLocation()));
-             }
-        }
+	public String updateConfiguration(List<OctaneConfigStructure> newConfigs) {
+		List<OctaneConfigStructure> originalConfigs = holder.getConfigs();
+		for (OctaneConfigStructure newConf : newConfigs) {
+			OctaneConfigStructure result = originalConfigs.stream()
+					.filter(or_conf -> or_conf.getIdentity().equals(newConf.getIdentity()))
+					.findAny()
+					.orElse(null);
+			if (result == null) {
+				MqmProject project = checkAndUpdateIdentityAndLocationIfNotTheSame(newConf);
+				String sp = project.getSharedSpace();
+				if (project.hasError()) {
+					return buildResponseStringEmptyConfigs(project.getErrorMsg());
+				}
+				OctaneConfiguration octaneConfiguration = new OctaneConfiguration(newConf.getIdentity(), newConf.getLocation(),
+						project.getSharedSpace());
+				octaneConfiguration.setClient(newConf.getUsername());
+				octaneConfiguration.setSecret(newConf.getSecretPassword());
+				try {
+					OctaneSDK.addClient(octaneConfiguration, TeamCityPluginServicesImpl.class);
+				} catch (Exception e) {
+					return buildResponseStringEmptyConfigs(e.getMessage());
+				}
+				holder.getOctaneConfigurations().put(newConf.getIdentity(), octaneConfiguration);
+				newConf.setSharedSpace(sp);
+				holder.getConfigs().add(newConf);
+			} else {
+				//update existing configuration
+				OctaneConfiguration octaneConfiguration = holder.getOctaneConfigurations().get(result.getIdentity());
+				MqmProject project = parseUiLocation(newConf.getUiLocation());
+				if (project.hasError()) {
+					return buildResponseStringEmptyConfigs(project.getErrorMsg());
+				}
+				String sp = project.getSharedSpace();
+				String location = project.getLocation();
+				octaneConfiguration.setUrl(newConf.getUiLocation());
+				result.setUiLocation(newConf.getUiLocation());
+				octaneConfiguration.setClient(newConf.getUsername());
+				result.setUsername(newConf.getUsername());
+				octaneConfiguration.setSecret(newConf.getSecretPassword());
+				result.setSecretPassword(newConf.getSecretPassword());
+				octaneConfiguration.setSharedSpace(sp);
+				result.setSharedSpace(sp);
+				result.setLocation(location);
+			}
+		}
 
-        return save();
-    }
+		return save();
+	}
 
-    private void checkAndUpdateIdentityAndLocationIfNotTheSame(OctaneConfigStructure newConf) {
-        String identity = newConf.getIdentity();
-        String location = parseUiLocation(newConf.getUiLocation());
-        newConf.setLocation(location);
-        if (holder.getConfigs().contains(newConf)){
-            OctaneConfigStructure matchingObject = holder.getConfigs().stream().
-                    filter(c -> c.equals(newConf)).
-                    findAny().orElse(null);
-            if (matchingObject != null){
-                newConf.setIdentity(matchingObject.getIdentity());
-                newConf.setIdentityFrom(matchingObject.getIdentityFrom());
-                return;
-            }
-        }
-        if (identity == null || identity.equals("") || "undefined".equalsIgnoreCase(identity)) {
-            newConf.setIdentity(UUID.randomUUID().toString());
-            newConf.setIdentityFrom(String.valueOf(new Date().getTime()));
-        }
-    }
+	private MqmProject checkAndUpdateIdentityAndLocationIfNotTheSame(OctaneConfigStructure newConf) {
+		String identity = newConf.getIdentity();
+		MqmProject project = parseUiLocation(newConf.getUiLocation());
+		if (project.hasError()) {
+			return project;
+		}
+		String location = project.getLocation();
+		String sp = project.getSharedSpace();
+		newConf.setLocation(location);
+		if (holder.getConfigs().contains(newConf)) {
+			OctaneConfigStructure matchingObject = holder.getConfigs().stream().
+					filter(c -> c.equals(newConf)).
+					findAny().orElse(null);
+			if (matchingObject != null) {
+				newConf.setIdentity(matchingObject.getIdentity());
+				newConf.setIdentityFrom(matchingObject.getIdentityFrom());
+				return project;
+			}
+		}
+		if (identity == null || identity.equals("") || "undefined".equalsIgnoreCase(identity)) {
+			newConf.setIdentity(UUID.randomUUID().toString());
+			newConf.setIdentityFrom(String.valueOf(new Date().getTime()));
+		}
+		return project;
+	}
 
-    public String reloadConfiguration() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            List<OctaneConfigStructure> cfg = holder.getConfigs();
-            return mapper.writeValueAsString(cfg);
-        } catch (JsonProcessingException jpe) {
-            logger.error("failed to reload configuration", jpe);
-            return "failed to reload configuration";
-        }
-    }
+	public String reloadConfiguration() {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			List<OctaneConfigStructure> cfg = holder.getConfigs();
+			return mapper.writeValueAsString(cfg);
+		} catch (JsonProcessingException jpe) {
+			logger.error("failed to reload configuration", jpe);
+			return "failed to reload configuration";
+		}
+	}
 
-    public String parseUiLocation(String uiLocation) {
-        int contextPos = uiLocation.indexOf("/ui");
-        if (contextPos < 0) {
-            return uiLocation;
-        } else {
-            return uiLocation.substring(0, contextPos);
-        }
-    }
+	public MqmProject parseUiLocation(String uiLocation) {
+		try {
+			URL url = new URL(uiLocation);
+			String location;
+			int contextPos = uiLocation.indexOf("/ui");
+			if (contextPos < 0) {
+				return new MqmProject("Application context not found in URL");
+			} else {
+				location = uiLocation.substring(0, contextPos);
+			}
+			List<NameValuePair> params = URLEncodedUtils.parse(url.toURI(), "UTF-8");
+			for (NameValuePair param : params) {
+				if (param.getName().equals(PARAM_SHARED_SPACE)) {
+					String[] sharedSpaceAndWorkspace = param.getValue().split("/");
+					if (sharedSpaceAndWorkspace.length < 1 || StringUtils.isEmpty(sharedSpaceAndWorkspace[0])) {
+						return new MqmProject("Unexpected shared space parameter value");
+					}
+					return new MqmProject(location, sharedSpaceAndWorkspace[0]);
+				}
+			}
+			return new MqmProject("Missing shared space parameter");
+		} catch (MalformedURLException e) {
+			logger.error(e.getMessage());
+			return new MqmProject("Invalid URL");
+		} catch (URISyntaxException e) {
+			logger.error(e.getMessage());
+			return new MqmProject("Invalid URL");
+		}
+	}
 }
