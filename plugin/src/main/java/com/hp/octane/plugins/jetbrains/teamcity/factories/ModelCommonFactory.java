8d64399ea5ebb7be395bbfb470c4e129c3ce9fd8
lokshin <evgene.lokshin@microfocus.com>
2019-08-06 12:01:14 +0300

    Defect #828193:  [Team City] Testing framework support Abort/Cancel.

--- plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/factories/ModelCommonFactory.java
+++ plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/factories/ModelCommonFactory.java
@@ -37,90 +37,90 @@ import java.util.List;
  */
 
 public class ModelCommonFactory {
-    private static final DTOFactory dtoFactory = DTOFactory.getInstance();
+	private static final DTOFactory dtoFactory = DTOFactory.getInstance();
 
-    @Autowired
-    private TCPluginParametersFactory parametersFactory;
-    @Autowired
-    private ProjectManager projectManager;
+	@Autowired
+	private TCPluginParametersFactory parametersFactory;
+	@Autowired
+	private ProjectManager projectManager;
 
-    public CIJobsList createProjectList() {
-        CIJobsList ciJobsList = dtoFactory.newDTO(CIJobsList.class);
-        List<PipelineNode> list = new ArrayList<>();
-        List<String> ids = new ArrayList<>();
+	public CIJobsList createProjectList() {
+		CIJobsList ciJobsList = dtoFactory.newDTO(CIJobsList.class);
+		List<PipelineNode> list = new ArrayList<>();
+		List<String> ids = new ArrayList<>();
 
-        PipelineNode buildConf;
-        for (SProject project : projectManager.getProjects()) {
+		PipelineNode buildConf;
+		for (SProject project : projectManager.getProjects()) {
 
-            List<SBuildType> buildTypes = project.getBuildTypes();
-            for (SBuildType buildType : buildTypes) {
-                if (!ids.contains(buildType.getInternalId())) {
-                    ids.add(buildType.getInternalId());
-                    buildConf = dtoFactory.newDTO(PipelineNode.class)
-                            .setJobCiId(buildType.getExternalId())
-                            .setName(buildType.getName())
-                            .setParameters(parametersFactory.obtainFromBuildType(buildType));
-                    list.add(buildConf);
-                }
-            }
-        }
+			List<SBuildType> buildTypes = project.getBuildTypes();
+			for (SBuildType buildType : buildTypes) {
+				if (!ids.contains(buildType.getInternalId())) {
+					ids.add(buildType.getInternalId());
+					buildConf = dtoFactory.newDTO(PipelineNode.class)
+							.setJobCiId(buildType.getExternalId())
+							.setName(buildType.getName())
+							.setParameters(parametersFactory.obtainFromBuildType(buildType));
+					list.add(buildConf);
+				}
+			}
+		}
 
-        ciJobsList.setJobs(list.toArray(new PipelineNode[list.size()]));
-        return ciJobsList;
-    }
+		ciJobsList.setJobs(list.toArray(new PipelineNode[list.size()]));
+		return ciJobsList;
+	}
 
-    public PipelineNode createStructure(String buildConfigurationId) {
-        SBuildType root = projectManager.findBuildTypeByExternalId(buildConfigurationId);
-        PipelineNode treeRoot = null;
-        if (root != null) {
-            treeRoot = dtoFactory.newDTO(PipelineNode.class)
-                    .setJobCiId(root.getExternalId())
-                    .setName(root.getName())
-                    .setParameters(parametersFactory.obtainFromBuildType(root));
+	public PipelineNode createStructure(String buildConfigurationId) {
+		SBuildType root = projectManager.findBuildTypeByExternalId(buildConfigurationId);
+		PipelineNode treeRoot = null;
+		if (root != null) {
+			treeRoot = dtoFactory.newDTO(PipelineNode.class)
+					.setJobCiId(root.getExternalId())
+					.setName(root.getName())
+					.setParameters(parametersFactory.obtainFromBuildType(root));
 
-            List<PipelineNode> pipelineNodeList = buildFromDependenciesFlat(root.getOwnDependencies());
-            if (!pipelineNodeList.isEmpty()) {
-                PipelinePhase phase = dtoFactory.newDTO(PipelinePhase.class)
-                        .setName("teamcity_dependencies")
-                        .setBlocking(true)
-                        .setJobs(pipelineNodeList);
-                List<PipelinePhase> pipelinePhaseList = new ArrayList<PipelinePhase>();
-                pipelinePhaseList.add(phase);
-                treeRoot.setPhasesPostBuild(pipelinePhaseList);
-            }
-        } else {
-            //should update the response?
-        }
-        return treeRoot;
-    }
+			List<PipelineNode> pipelineNodeList = buildFromDependenciesFlat(root.getOwnDependencies());
+			if (!pipelineNodeList.isEmpty()) {
+				PipelinePhase phase = dtoFactory.newDTO(PipelinePhase.class)
+						.setName("teamcity_dependencies")
+						.setBlocking(true)
+						.setJobs(pipelineNodeList);
+				List<PipelinePhase> pipelinePhaseList = new ArrayList<PipelinePhase>();
+				pipelinePhaseList.add(phase);
+				treeRoot.setPhasesPostBuild(pipelinePhaseList);
+			}
+		} else {
+			//should update the response?
+		}
+		return treeRoot;
+	}
 
-    private List<PipelineNode> buildFromDependenciesFlat(List<Dependency> dependencies) {
-        List<PipelineNode> result = new LinkedList<>();
-        if (dependencies != null) {
-            for (Dependency dependency : dependencies) {
-                SBuildType build = dependency.getDependOn();
-                if (build != null) {
-                    PipelineNode buildItem = dtoFactory.newDTO(PipelineNode.class)
-                            .setJobCiId(build.getExternalId())
-                            .setName(build.getName())
-                            .setParameters(parametersFactory.obtainFromBuildType(build));
-                    result.add(buildItem);
-                    result.addAll(buildFromDependenciesFlat(build.getOwnDependencies()));
-                }
-            }
-        }
-        return result;
-    }
+	private List<PipelineNode> buildFromDependenciesFlat(List<Dependency> dependencies) {
+		List<PipelineNode> result = new LinkedList<>();
+		if (dependencies != null) {
+			for (Dependency dependency : dependencies) {
+				SBuildType build = dependency.getDependOn();
+				if (build != null) {
+					PipelineNode buildItem = dtoFactory.newDTO(PipelineNode.class)
+							.setJobCiId(build.getExternalId())
+							.setName(build.getName())
+							.setParameters(parametersFactory.obtainFromBuildType(build));
+					result.add(buildItem);
+					result.addAll(buildFromDependenciesFlat(build.getOwnDependencies()));
+				}
+			}
+		}
+		return result;
+	}
 
-    public CIBuildResult resultFromNativeStatus(Status status) {
-        CIBuildResult result = CIBuildResult.UNAVAILABLE;
-        if (status == Status.ERROR || status == Status.FAILURE) {
-            result = CIBuildResult.FAILURE;
-        } else if (status == Status.WARNING) {
-            result = CIBuildResult.UNSTABLE;
-        } else if (status == Status.NORMAL) {
-            result = CIBuildResult.SUCCESS;
-        }
-        return result;
-    }
+	public CIBuildResult resultFromNativeStatus(Status status, boolean isInterrupted) {
+		CIBuildResult result = isInterrupted ? CIBuildResult.ABORTED : CIBuildResult.UNAVAILABLE;
+		if (status == Status.ERROR || status == Status.FAILURE) {
+			result = CIBuildResult.FAILURE;
+		} else if (status == Status.WARNING) {
+			result = CIBuildResult.UNSTABLE;
+		} else if (status == Status.NORMAL) {
+			result = CIBuildResult.SUCCESS;
+		}
+		return result;
+	}
 }
